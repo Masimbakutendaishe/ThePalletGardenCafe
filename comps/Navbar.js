e085a5889bfcd0abd3620f526a842cc2aa197a3d
Masimbakutendaishe <49595768+Masimbakutendaishe@users.noreply.github.com>
2024-11-27 21:06:15 +0200

    Update Navbar.js - Food Menu and Drinks Menu Links

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -63,7 +63,7 @@ const Navbar = () => {
                   <a href="/FoodMenuPage">Food Menu</a>
                 </li>
                 <li className="px-4 py-2 hover:bg-gray-200 transition-all">
-                  <a href="/drinks-menu">Drinks Menu</a>
+                  <a href="/DrinksMenuPage">Drinks Menu</a>
                 </li>
                 <li className="px-4 py-2 hover:bg-gray-200 transition-all">
                   <a href="/place-order">Place Order</a>

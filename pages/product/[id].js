16c4bc0da5fd8f04639429ba78b941da9dd145dc
Masimbakutendaishe <49595768+Masimbakutendaishe@users.noreply.github.com>
2024-12-14 01:15:53 +0200

    Update [id].js

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -4,8 +4,8 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { addProduct } from "../../redux/cartSlice";
 
-const Product = ({ pizza }) => {
-  const [price, setPrice] = useState(pizza.prices[0]);
+const Product = ({ item }) => {
+  const [price, setPrice] = useState(item.prices[0]);
   const [size, setSize] = useState(0);
   const [quantity, setQuantity] = useState(1);
   const [extras, setExtras] = useState([]);
@@ -16,7 +16,7 @@ const Product = ({ pizza }) => {
   };
 
   const handleSize = (sizeIndex) => {
-    const difference = pizza.prices[sizeIndex] - pizza.prices[size];
+    const difference = item.prices[sizeIndex] - item.prices[size];
     setSize(sizeIndex);
     changePrice(difference);
   };
@@ -34,25 +34,25 @@ const Product = ({ pizza }) => {
   };
 
   const handleClick = () => {
-    dispatch(addProduct({ ...pizza, extras, price, quantity }));
+    dispatch(addProduct({ ...item, extras, price, quantity }));
   };
 
   return (
     <div className="flex flex-col md:flex-row md:h-[calc(100vh-100px)]">
       <div className="flex-1 flex items-center justify-center">
         <div className="relative w-4/5 h-4/5">
-          <Image src={pizza.img} alt="Pizza" layout="fill" objectFit="contain" />
+          <Image src={item.img} alt="Item" layout="fill" objectFit="contain" />
         </div>
       </div>
       <div className="flex-1 p-5">
-        <h1 className="text-2xl font-bold mb-4">{pizza.title}</h1>
+        <h1 className="text-2xl font-bold mb-4">{item.title}</h1>
         <span className="text-lg font-medium text-red-600 border-b border-red-600 mb-4 block">
           ${price}
         </span>
-        <p className="mb-6">{pizza.desc}</p>
+        <p className="mb-6">{item.desc}</p>
         <h3 className="text-lg font-semibold mb-3">Choose the size</h3>
         <div className="flex space-x-4 mb-6">
-          {pizza.prices.map((_, index) => (
+          {item.prices.map((_, index) => (
             <div
               key={index}
               className={`relative cursor-pointer ${index === 1 ? "w-10 h-10" : "w-8 h-8"} ${
@@ -69,7 +69,7 @@ const Product = ({ pizza }) => {
         </div>
         <h3 className="text-lg font-semibold mb-3">Choose additional ingredients</h3>
         <div className="flex flex-wrap gap-4 mb-6">
-          {pizza.extraOptions.map((option) => (
+          {item.extraOptions.map((option) => (
             <div className="flex items-center space-x-2" key={option._id}>
               <input
                 type="checkbox"
@@ -108,7 +108,7 @@ export const getServerSideProps = async ({ params }) => {
   const res = await axios.get(`http://localhost:3000/api/products/${params.id}`);
   return {
     props: {
-      pizza: res.data,
+      item: res.data,
     },
   };
 };

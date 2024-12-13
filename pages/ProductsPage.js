8b62de39caf0f9e4e9a9d9400dc824cc59845282
Masimbakutendaishe <49595768+Masimbakutendaishe@users.noreply.github.com>
2024-12-14 01:17:06 +0200

    Update ProductsPage.js

--- pages/ProductsPage.js
+++ pages/ProductsPage.js
@@ -3,22 +3,22 @@ import Head from "next/head";
 import { useState } from "react";
 import Add from "../comps/Add";
 import AddButton from "../comps/AddButton";
-import PizzaList from "../comps/PizzaList";
+import ItemList from "../comps/ItemList";
 
-export default function ProductsPage({ pizzaList, admin }) {
+export default function ProductsPage({ itemList, admin }) {
   const [close, setClose] = useState(true);
   
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center py-8">
       <Head>
-        <title>Pizza Products</title>
-        <meta name="description" content="Browse our delicious pizzas" />
+        <title>Meals On offer</title>
+        <meta name="description" content="Browse our delicious meals" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
       {admin && <AddButton setClose={setClose} />}
       
-      <PizzaList pizzaList={pizzaList} />
+      <ItemList itemList={itemList} />
       
       {!close && <Add setClose={setClose} />}
     </div>
@@ -36,7 +36,7 @@ export const getServerSideProps = async (ctx) => {
   const res = await axios.get("http://localhost:3000/api/products");
   return {
     props: {
-      pizzaList: res.data,
+      itemList: res.data,
       admin,
     },
   };

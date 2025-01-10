b1a253922768e7e70f25b4eddaf244efa5b480b7
Masimbakutendaishe <49595768+Masimbakutendaishe@users.noreply.github.com>
2025-01-10 23:06:13 +0200

    Update DrinksMenuPage.js

--- pages/DrinksMenuPage.js
+++ pages/DrinksMenuPage.js
@@ -124,7 +124,7 @@ const MenuItems = () => {
 
       {/* Fun Fact Section */}
       <section className="text-center mb-12">
-        <h3 className="text-2xl font-bold mb-4">Today's Fun Fact!</h3>
+        <h3 className="text-2xl font-bold mb-4">Today&apos;s Fun Fact!</h3>
         <p className="text-lg mb-4">Did you know? Drinking water can help improve your skin health!</p>
         <img src="/drink7.jpg" alt="Water" className="mx-auto w-32 h-32 object-cover rounded-full" />
       </section>
